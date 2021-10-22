54dc6094b7b86eb8effb8184f0490bff40f09ff2
aditya <adityalipu78@gmail.com>
2021-10-22 19:06:42 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjBiKfpyIDcoVodyYILodAW7qWU_JBoqE",
+  authDomain: "spacex-api-e58c5.firebaseapp.com",
+  projectId: "spacex-api-e58c5",
+  storageBucket: "spacex-api-e58c5.appspot.com",
+  messagingSenderId: "266913901179",
+  appId: "1:266913901179:web:5d9c9734345bb8fa2050de",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
